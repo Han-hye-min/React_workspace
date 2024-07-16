e8f7a592fa9cc0224e5024a5c8c7efc158eb8cee
Han-hye-min <hhan010215@gmail.com>
2024-07-16 21:23:09 +0900

    react_workspace

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  //함수 안에서 return 이전은 모두 스크립트 영역
+
+  return (
+    <div className="App">
+      {/* html 안에서는 자바스크립트 주석 사용 가능 */}
+      <h1>안녕하세요</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Hello(props) {
+
+    let count = 1;
+
+    return (
+        <div>
+           <h1> {count} </h1> 
+        </div>
+    );
+}
+
+export default Hello;
